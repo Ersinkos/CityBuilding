7255cae71556a3eb707477753d4289b799996686
Ersin Köseoğlu <63159627+Ersinkos@users.noreply.github.com>
2024-04-28 10:49:51 +0300

    UI resource icons

---
+++ CityBuilding/Assets/Scripts/Definitions/ResourceSO.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceType
+{
+	Water,
+	Iron,
+	Energy,
+	Money,
+}
+[CreateAssetMenu(menuName = "ScriptableObjects/Resource")]
+public class ResourceSO : ScriptableObject
+{
+	public ResourceType resourceType;
+	public string nameString;
+}
